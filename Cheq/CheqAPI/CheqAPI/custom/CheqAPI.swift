ccef78dcd74a7feed4d5c28532361e6ef33ee552
XUWEI LIANG <email.xuwei@gmail.com>
2019-08-18 23:34:03 +1000

    added generated api stub from swagger

---
+++ Cheq/CheqAPI/CheqAPI/custom/CheqAPI.swift
@@ -0,0 +1,16 @@
+//
+//  CheqAPI.swift
+//  CheqAPI
+//
+//  Created by XUWEI LIANG on 18/8/19.
+//  Copyright © 2019 WiseTree Solutions Pty Ltd. All rights reserved.
+//
+
+public class CheqAPI {
+
+    public init() {}
+    
+    public func helloWorld()-> String {
+        return "Hello World" 
+    }
+}
--- Cheq/CheqTests/CheqTests.swift
+++ Cheq/CheqTests/CheqTests.swift
@@ -7,28 +7,15 @@
 //
 
 import XCTest
-@testable import CheqDemo
+import CheqAPI
+@testable import Cheq
 
-class CheqDemoTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+class CheqAPITests: XCTestCase {
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let api = CheqAPI()
+        let result = api.helloWorld()
+        XCTAssertNotNil(result)
+        XCTAssertTrue(result == "Hello World")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
