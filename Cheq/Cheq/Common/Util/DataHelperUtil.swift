61ef62a2ba5066e8deceb3e6b66db07abdac77eb
iTelaSoft-PC <danutha.fernando@itelasoft.com>
2020-03-23 15:07:01 +0530

    Refactored

--- Cheq/Cheq/Common/Util/DataHelperUtil.swift
+++ Cheq/Cheq/Common/Util/DataHelperUtil.swift
@@ -226,3 +226,15 @@ class DataHelperUtil {
         }
     }
 }
+//Converts a struct in to a dictionary
+struct JSON {
+    static let encoder = JSONEncoder()
+}
+extension Encodable {
+    subscript(key: String) -> Any? {
+        return dictionary[key]
+    }
+    var dictionary: [String: Any] {
+        return (try? JSONSerialization.jsonObject(with: JSON.encoder.encode(self))) as? [String: Any] ?? [:]
+    }
+}
--- Cheq/Cheq/Service/Intercom/IntercomManager.swift
+++ Cheq/Cheq/Service/Intercom/IntercomManager.swift
@@ -64,16 +64,4 @@ struct IntercomPersonalData:Codable {
     
 }
 
-//Converts a struct in to a dictionary
-struct JSON {
-    static let encoder = JSONEncoder()
-}
-extension Encodable {
-    subscript(key: String) -> Any? {
-        return dictionary[key]
-    }
-    var dictionary: [String: Any] {
-        return (try? JSONSerialization.jsonObject(with: JSON.encoder.encode(self))) as? [String: Any] ?? [:]
-    }
-}
 
